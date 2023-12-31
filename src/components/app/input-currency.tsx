77a7026ae31bab90444bb6054c1ca9be545c54fe
agfebrian <febriananggi9@gmail.com>
2023-12-31 16:41:29 +0700

    refactor: remove code unused

--- src/components/app/input-currency.tsx
+++
@@ -1,6 +0,0 @@
-import { NumericFormat } from "react-number-format"
-import { Input } from "."
-
-export const InputCurrency = ({ ...rest }) => {
-  return <NumericFormat customInput={Input} {...rest} />
-}
